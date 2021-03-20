71b2bfd51724d9e0f9e0036380563dc24dabaa03
muriga <milos.uriga313@gmail.com>
2021-03-20 08:50:15 +0100

    documentation improved

--- src/rpvs2/ocr.py
+++ src/rpvs2/ocr.py
@@ -26,15 +26,17 @@ from pytesseract import pytesseract
 
 
 def convert_to_text(file: Union[str, fitz.Document], save: bool = False):
-    """Gets file or path to file, convert its to string and if save is set to True,
-        save string to txt file with same name as pdf have. Method also returns that string.
+    """Gets file or path to file, convert its to string and if save is set to True, save string
+    to txt file with same name as pdf have. Method also returns that string.
 
-    :param file: str or fitz.Document
-        The file opened by fitz or path to the file
-    :param save: bool, optional
-        Determine if string should be saved. Default is false.
-    :returns text: str
-        Returns recognized text
+
+    :param file: The file opened by fitz or path to the file
+    :type file: Union[str, fitz.Document]
+    :param save: Determine if string should be saved. Default is false.
+    :type save: bool
+    :returns: Recognized text
+    :rtype: str
+
     """
     text = ""
     if type(file) == str:
@@ -50,15 +52,17 @@ def convert_to_text(file: Union[str, fitz.Document], save: bool = False):
     return text
 
 
-def iterate_folder_convert_to_text(folder: str, destination: dict = None):
+def iterate_folder_convert_to_text(folder: str, save: bool = False):
     """Gets file to folder, then iterate thought every pdf file in this folder, convert
         it to string. Then save text to given dict or when no dict was given, to txt files
         in same folder.
 
-    :param folder: str
-        Path to folder with pdf files which should be converted to text.
-    :param destination: dict, optional
-        Dictionary where output should be saved
+    :param folder: Path to folder with pdf files which should be converted to text.
+    :type folder: str
+    :param save: Determine if strings should be saved. Default is false.
+    :type save: bool
+    :returns: A dictionary where key is name of file and value recognized text
+    :rtype: dict
     """
     pass
 
