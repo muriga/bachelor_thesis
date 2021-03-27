3921f55b71e494c52537c7b98870e2e4695c3174
muriga <milos.uriga313@gmail.com>
2021-03-27 18:01:16 +0100

    redudant code deleted, misclassified pdf from pdf replaced

--- src/rpvs2/pattern.py
+++ src/rpvs2/pattern.py
@@ -135,44 +135,17 @@ class PatternExtract(Classifier):
 
     def get_patterns(self):
         patterns = []
-        #patterns.append(re.compile(r'podmienka zápis členov vrcholového manažment '
-        #                           r'ust \. [$8§] 4 ods[a-zA-Z0-9.,§$ ]{0,80} byť splnené'))  # 0.
-        #patterns.append(re.compile("spoločnosť (výlučne )?(nepriamo )?ovládanej emitentom cenných papier"))  # 1
-        #patterns.append(re.compile("nebyť (oprávnenou osobou )?(os )?identifikovaná žiaden fyzický osoba( ,)? mať 25%"))  # 2
-        #patterns.append(re.compile("zapisujú namiesto k[uú]v člen(ov)? vrcholového manažment"))  # 3
-        #patterns.append(re.compile("neexistovať žiaden osoba( ,)? konať zhode spoločným postupom( ,)? žiaden osoba( ,"
-        #                           ")? pvs ovláda"))  # 4
-        #patterns.append(re.compile("osoba [8$§]? 6a ods \. 2 zák \. č \. 297\/2008")) # 5
-        #patterns.append(re.compile(r'nie byť žiaden fyzický osoba( ,)? zmysel ustanovenie '
-        #                           r'zakx mať priamy nepriamy podiel')) # 6
-        #patterns.append(re.compile(r'neexistovať žiaden fyzický osoba( alebo akcionár)?'
-        #                           r'( ,)? mať priamy nepriamy podiel (alebo súčet )?(najmenej )?25%')) # 7
-        #patterns.append(re.compile(r'žiaden fyzický osoba nespĺňa (definícium|kritéria)?'))# (konečného|KUV)')) # 8
-        #patterns.append(re.compile(r'namiesto konečných užívateľ výhod zapisuje'))  # This should be better preprocessed look at #3
-        #patterns.append(re.compile(r'neidentifikoval(a)? žiadny fyzické osoba kuv')) # 10
-        #patterns.append(re.compile(r'z[aá]pis [cč]len vrcholov[eé]ho mana[zž]ment(u)?([^A-W ]*)(s[úu]|byť) splnen[eé]')) # 11
-        #patterns.append(re.compile(r'[čc]len(ovia)? vrcholového')) # 0
         patterns.append(re.compile("(spoločnosť|pvs) (výlučne )?(nepriamo )?ovládanej emitentom"))  # 0
-        patterns.append(re.compile("zapisuj[^ \n] namiesto kuv"))  # 1
-        patterns.append(re.compile("osoba [8$§]? 6a ods \. 2 zák \. č \. 297\/2008")) #2
-        patterns.append(re.compile(r'(nofo nesp..a|nesp..a nofo)( |\n)((u) pvs )?(defin.ciu[^ \n]*|krit.ri[^ \n]*|([^ \n]*( |\n)){0,3})kuv'))#3
-        patterns.append(re.compile(r'namiesto kuv (zapisuje|pova.[^ \n]*)'))  #4
-        patterns.append(re.compile(r'namiesto kuv')) #5
-        patterns.append(re.compile(r'6a[^ \n]*ods.[^ \n]*2[^ \n]*z.kona[^ \n]*.[^ \n]*297/2008')) #6
-        patterns.append(re.compile(r'nofo( |\n)*(pvs( |\n)*)*nesp..a( |\n)*(krit.r(^ \n)*|podmienk(^ \n)*)'))#7
-        patterns.append(re.compile(r'kuv( |\n)*(spolo([^ \n]*)?( |\n))?pov([^ \n]*)? vmkuv'))#8
-        patterns.append(re.compile(r'pov([^ \n]*)? vmkuv'))#9
-        #pvs nie byť pvs
+        patterns.append(re.compile("osoba [8$§]? 6a ods \. 2 zák \. č \. 297\/2008")) #1
+        patterns.append(re.compile(r'(nofo nesp..a|nesp..a nofo)( |\n)((u) pvs )?(defin.ciu[^ \n]*|krit.ri[^ \n]*|([^ \n]*( |\n)){0,3})kuv'))#2
+        patterns.append(re.compile(r'namiesto kuv (zapisuje|pova.[^ \n]*)'))  #3
+        patterns.append(re.compile(r'namiesto kuv')) #4
+        patterns.append(re.compile(r'nofo( |\n)*(pvs( |\n)*)*nesp..a( |\n)*(krit.r(^ \n)*|podmienk(^ \n)*)'))#5
+        patterns.append(re.compile(r'kuv( |\n)*(spolo([^ \n]*)?( |\n))?pov([^ \n]*)? vmkuv'))#6
+        patterns.append(re.compile(r'pov([^ \n]*)? vmkuv'))#7
         return patterns
 
     def get_setting_patterns(self):
-        # chcelo by to predtym zmenit veci typu konečný užívateľ výhod/KÚV na KUV
-        # namiesto KUV niečo
-        # zapisujú namiesto konečných užívateľov výhod štatutárny orgán #9,3
-        # sú splnené podmienky na zápis členov štatutárneho orgánu do registra namiesto konečného užívateľa výhod #1,3,11
-        # namiesto konečného užívateľa výhod zapisujú členovia jeho štatutárneho orgánu #9,3
-        # budú v súlade s § 4 odsek 5 zákona č. 315/2016 o registri partnerov verejného sektora
-        # zapísaný členovia predstavenstva štatutárneho orgánu PVS    #zapisany clenovia predstavenstva?
         patterns = []
         patterns.append("podmienky na zápis členov vrcholového manažmentu podľa ust. § 4 ods. ... sú splnené")
         patterns.append("ako spoločnosti (nepriamo) ovládanej emitentom cenných papierov")
@@ -190,61 +163,6 @@ class PatternExtract(Classifier):
         patterns.append(r'zápis členov vrcholového manažmentu sú splnené')
         patterns.append(r'členovia vrcholového')
         return patterns
-
-    def test2(self):
-        text = "žiadna fyzická osoby nespíňala kritériá pre KÚV\n"\
-        "KÚV vmkuv\n"\
-        "----\n"\
-        "žiadna fyzická osoba nespĺňa kritériá\n"\
-        "za konečných užívateľov výhod PVS v zmysle § 6a ods. 2 zákona č.297/2008 Z.z. považujú vmkuv\n"\
-        "---\n"\
-        "pričom žiaden z akcionárov VEOLIA ENVIRRONEMENT-VE nevlastní 25 % a viac akcií\n"\
-        "neexistuje fyzická osoba, ktorá disponuje právom (nepriamym) na hospodársky prospech najmenej 25 % z podnikania Partnera verejného sektora\n"\
-        "---\n"\
-        "žiadna fyzická osoba v PVS nesplňa kritéria uvedené v 8 6a odsek 1 písm. a) zákona č. 297/2008 Z.z. o ochrane pred legalizáciou príjmov z trestnej činnosti a:o ochrane pred financovaním terorizmu a o zmene a doplnení niektorých zákonov\n"\
-        "za konečného užívateľa výhod považujú vmkuv PVS\n"\
-        "---\n"\
-        "sa nepodarilo identifikovať takú fyzickú osobu, resp. fyzické osoby, ktoré by spĺňali kritériá uvedené v  §6a ods. 1, písm. a)" \
-        "V nadväznosti na uvedené ustanovenie sa majú podľa § 6a ods. 2 Zákona o ochrane pred legalizáciou príjmov z trestnej činnosti za konečných uţívateľov výhod Spoločnosti povaţovať vmkuv\n"\
-        "---\n"\
-        "Za končeného užívateľa výhod u Partnera verejného sektora sa preto vzhľadom k ust. $ 6a ods. 2\n"\
-        "zákona o ochrane pred legalizáciou príjmov z trestnej činnosti považujú vmkuv. Za konečných užívateľov výhod sa považuje vmkuv, alebo vmkuv\n"\
-        "---\n"\
-        "žiadna fyzická osoba nespĺňa kritériá uvedené v odseku 8, za konečných užívateľov výhod sa \n"\
-        "považujú členovia jej vrcholového manažmentu\n"\
-        "---\n"\
-        "PVS je spoločnosť, ktorú nepriamo a výlučne majetkovo ovláda a nepriamo a výlučne riadi spoločnosť, ktorá je emitentom cenných\n"\
-        "do registra partnerov verejného sektora budú v súlade s § 4 odsek 5 zákona č. 315/2016 o registri partnerov verejného sektora zapísaný členovia predstavenstva štatutárneho orgánu PVS\n"\
-        "---\n"\
-        "že žiadna fyzická osoba nespíňa kritériá uvedené v 8 6a odsek 1 písm. a) zákona č. 297/2008 Z. z. o ochrane pred legalizáciou príjmov z trestnej činnosti a o ochrane pred financovaním terorizmu a o zmene a doplnení niektorých zákonov v znení neskorších predpisov \n"\
-        "Za konečných užívateľov výhod PVS sa preto vzhľadom k 8 6a ods. 2 zákona o ochrane pred legalizáciou príjmov z trestnej činnosti považujú členovia jej vrcholového manažmentu\n"\
-        "---\n"\
-        "Vzhľadom na skutočnosť, že na základe predložených dokumentov a overených informácií\n"\
-        "nespíňa žiadna osoba u partnera verejného sektora kritériá pre konečného užívateľa výhod\n"\
-        "v zmysle 86a ods. 1 zákona č. 297/2008 Z. z. o ochrane pred legalizáciou príjmov z trestnej\n"\
-        "činnosti, považujú sa za konečných užívateľov výhod u partnera verejného sektora členovia jeho\n"\
-        "vrcholového manažmentu \n"\
-        "---\n"\
-        "žiadna fyzická osoba nespĺňa kritériá uvedené v § 6a ods. 1 písm. a) Zákona o ochrane pred legalizáciou príjmov z TČ, \n"\
-        "za konečných užívateľov výhod sa považujú členovia vrcholového manažmentu Partnera verejného sektora\n"\
-        "---\n"\
-        "žiadna fyzická osoba nespĺňa kritériá\n"\
-        "za konečných užívateľov výhod u PVS považujú členovia jeho vrcholového manažmentu\n"\
-        "---\n"\
-        "žiadna fyzická osoba nespíňa podmienku stanovenú ust. $ 6a ods. | aods. 3 Zákona oochrane pred legalizáciou príjmov z trestne\n"\
-        "žiadnu fyzickú osobu nemožno určiť ako konečného užívateľa výhod, nakoľko žiadna fyzická osoba nespíňa kritéria uvedené v ust. $ 6a ods. I písm. a) Zákona o ochrane pred legalizáciou príjmov z trestnej činnostij činnosti\n"\
-        "Za konečných užívateľov výhod u PYS sa budú v súlade s ust. 8 6a ods. 2 Zákona o ochrane príjmov z trestnej činnosti považovať členovia jej vrcholového manažmentu\n"\
-        "---\n"\
-        "neexistuje a nie je (im) známa žiadna fyzická osoba: -  ktorá by mala priamy podiel alebo ich súčet najmenej 25% na hlasovacích právach v Partnerovi verejného sektora\n"\
-        "neexistuje žiadna fyzická osoba, ktorá by spĺňala podmienky v zmysle § 6a ods.1 písm. a) zákona č. \n"\
-        "297/2008 Z.z., a preto v súlade s §6a ods.2 tohto zákona sa za konečného užívateľa výhod považujú členovia jej vrcholového manažmentu (t.z. štatutárny orgán, člen štatutárneho orgánu, prokurista a vedúci zamestnanec v priamej riadiacej pôsobnosti štatutárneho orgánu)\n"\
-        "---\n"\
-        "Žiadna fyzická osoba nespíňa kritériá uvedené v odseku 1 písm. a), zákona č. 279/2008 Z. z. a\n"\
-        "za konečných užívateľov výhod sa považujú členovia jej vrcholového manažmentu: za člena\n"\
-        "vrcholového manažmentu sa považuje štatutárny orgán, člen štatutárneho orgánu,\n"\
-        "prokurista a vedúci zamestnanec v priamej riadiacej pôsobnosti štatutárneho orgánu."
-        text = self.preprocessing(text)
-        print(self.tokenize(text))
 
     def test(self):
         strings = []
